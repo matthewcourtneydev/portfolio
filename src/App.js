26d8611c6e9f0e4cbb7860e2916a7a549c835010
Matthew Courtney <mdcdev23@gmail.com>
2023-08-17 23:25:58 -0400

    mdc_portfolio restyled aside

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-
-import './App.scss';
-import './App.css'
-import Navbar from './components/nav-bar';
-import HomePage from './components/home-page';
-import Aside from './components/aside';
+import "./App.scss";
+import "./App.css";
+import Navbar from "./components/nav-bar";
+import HomePage from "./components/home-page";
+import Aside from "./components/aside";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
       <Aside />
+      <Navbar />
       <HomePage />
     </div>
   );
--- src/components/nav-bar.jsx
+++ src/components/nav-bar.jsx
@@ -19,7 +19,7 @@ function Navbar() {
             <li className="nav-item"><a href="#experience" alt="experience">Experience</a></li>
             <li className="nav-item"><a href="#portfolio" alt="portfolio">Portfolio</a></li>
             <li className="nav-item"><a href="#contact" alt="contact">Contact</a></li>
-            <a href="https://docs.google.com/document/d/17opt3I8CcLb9RlvjImX0rZh_OCu8VfmVmiucvmh_AFQ/edit?usp=sharinghttps://docs.google.com/document/d/17opt3I8CcLb9RlvjImX0rZh_OCu8VfmVmiucvmh_AFQ/edit?usp=sharing" target='_blank' rel='noreferrer'><li className='resume-button'>Resume</li></a>
+            <a id="resume" href="https://docs.google.com/document/d/17opt3I8CcLb9RlvjImX0rZh_OCu8VfmVmiucvmh_AFQ/edit?usp=sharinghttps://docs.google.com/document/d/17opt3I8CcLb9RlvjImX0rZh_OCu8VfmVmiucvmh_AFQ/edit?usp=sharing" target='_blank' rel='noreferrer'><li className='resume-button'>Resume</li></a>
         </ul>
     </nav>   
     )
