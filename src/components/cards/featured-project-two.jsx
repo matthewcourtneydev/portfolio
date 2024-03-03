54fb7d705c9f5b8b69c383af0038e3eda1cd6ea5
Matthew Courtney <mdcdev23@gmail.com>
2024-03-03 01:48:37 -0500

    deployed

--- src/components/cards/featured-project-two.jsx
+++ src/components/cards/featured-project-two.jsx
@@ -1,35 +1,53 @@
-import React from 'react';
-import { UilExternalLinkAlt, UilGithubAlt } from '@iconscout/react-unicons'
+import React from "react";
+import { UilExternalLinkAlt, UilGithubAlt } from "@iconscout/react-unicons";
 import { useInView } from "react-intersection-observer";
 
 const ProjectTwo = () => {
-  const {ref: card, inView: cardVisable } = useInView();
+  const { ref: card, inView: cardVisable } = useInView();
 
   return (
-    <div ref={card} className={`card-container ${cardVisable ? "fade-in" : ""}`}>
-        <div className="new-background"></div>
-        <div className="project-card" id="project-two">
-          <p className="project-label">featured project</p>
-          <p className="project-title">Project One Title</p>
-          <p className="project-desc">
-          Exciting projects are on the horizon! Thank you for your patience as I diligently work on crafting high-quality additions to my portfolio. Stay tuned for updates and showcases of my latest work.
+    <div
+      ref={card}
+      className={`card-container ${cardVisable ? "fade-in" : ""}`}
+    >
+      <div className="new-background">
+        <div className="project-img2"></div>
+      </div>
+      <div className="project-card" id="project-two">
+        <p className="project-label">featured project</p>
+        <p className="project-title">Sorting Hat Quiz</p>
+        <p className="project-desc">
+          I created an engaging Sorting Hat Quiz using React.js, where users are
+          dynamically sorted into Hogwarts houses based on their responses to
+          tailored questions, drawing inspiration from the renowned Sorting Hat
+          of the Wizarding World.
         </p>
-          <ul className="project-tech">
-            <li className="tech">React</li>
-            <li className="tech">JSX</li>
-            <li className="tech">Scss</li>
-          </ul>
-          <div className="project-links-container">
-            <span className="btn">
+        <ul className="project-tech">
+          <li className="tech">React</li>
+          <li className="tech">JSX</li>
+          <li className="tech">Scss</li>
+        </ul>
+        <div className="project-links-container">
+          <span className="btn">
+            <a
+              href="https://matthewcourtneydev.github.io/sorting-hat-quiz/"
+              target="_blank"
+            >
               <UilExternalLinkAlt />
-            </span>
-            <span className="btn">
+            </a>
+          </span>
+          <span className="btn">
+            <a
+              href="https://github.com/matthewcourtneydev/sorting-hat-quiz"
+              target="_blank"
+            >
               <UilGithubAlt />
-            </span>
-          </div>
+            </a>
+          </span>
         </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default ProjectTwo;
